9f58420b37aff0ec1ff308ef6c602402aecd9e9f
Maxwell Margenot <max.margenot@gmail.com>
2016-06-16 08:29:18 -0400

    Laid groundwork for surfaces and implied vol calculation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import requests
 import json
 import re
-from utils import json_decode
+from utils import get_option_chain
  
 URL1 = 'http://www.google.com/finance/option_chain?q=%s&output=json'
 URL2 = 'http://www.google.com/finance/option_chain?q=%s&output=json&expy=%d&expm=%d&expd=%d' 
@@ -20,19 +20,6 @@ def main(argv):
     # Do some math
     # Generate a 3D plot on P x T x vol
 
-
-def get_option_chain(symbol):
-    """ Pulls the option chain from Google Finance.
-    Should return as a Pandas Panel """
-    url = URL1 % symbol
-    response = requests.get(url)
-    if response.status_code == 200:
-        content_json = response.content
-        content_json = content_json.decode('utf-8')
-        data = re.sub('(\w+:)(\d+\.?\d)', r'\1"\2"', content_json)
-        data = re.sub('(\w+):', r'"\1":', data)
-        data = json.loads(data)
-        return data
-        
+    
 if __name__ == "__main__":
     main(sys.argv)
---
+++ utils.py
@@ -0,0 +1,12 @@
+def get_option_chain(symbol):
+    """ Pulls the option chain from Google Finance.
+    Should return as a Pandas Panel """
+    url = URL1 % symbol
+    response = requests.get(url)
+    if response.status_code == 200:
+        content_json = response.content
+        content_json = content_json.decode('utf-8')
+        data = re.sub('(\w+:)(\d+\.?\d)', r'\1"\2"', content_json)
+        data = re.sub('(\w+):', r'"\1":', data)
+        data = json.loads(data)
+        return data
