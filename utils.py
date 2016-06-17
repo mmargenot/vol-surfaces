1afcc436de4e041fce5b2a24921fb3ebeb3400dd
Maxwell Margenot <max.margenot@gmail.com>
2016-06-17 08:31:06 -0400

    Laid groundwork for calculate_implied_vol

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ def get_option_chain(symbol, option_type = None, expiry_month = None, expiry_yea
     -------
     data : pandas.DataFrame
         DataFrame with MultiIndex that contains all
-        options listed for all expirations"""
+        options for all expirations """
         
     url = URL1 % symbol
     response = requests.get(url)
@@ -34,22 +34,25 @@ def get_option_chain(symbol, option_type = None, expiry_month = None, expiry_yea
     data = json.loads(data)
 
     # Parse data dict into a pd.DataFrame
-    def parse_dict(option_dict):
+    def parse_options_into_df(option_dict):
         """ Takes the option dictionary and turns it
         into a MultiIndex DataFrame """
         calls = option_dict['calls']
         calls = pd.DataFrame(calls)
+        # Reindex the calls
         calls = calls.set_index(calls['expiry'])
         calls = calls.drop('expiry', 1)
         
         puts = option_dict['puts']
         puts = pd.DataFrame(puts)
+        # Reindex the puts
         puts = puts.set_index(puts['expiry'])
         puts = puts.drop('expiry', 1)
         
         return pd.concat([calls, puts], keys = ['call', 'put'])
-        
-    return parse_dict(data)
+    
+    option_df = parse_options_into_df(data)
+    return option_df
     
 
         
@@ -59,11 +62,24 @@ def generate_plot(vol_frame):
     return
     
 def calculate_implied_vol(option_chain):
+    """ Uses the Black-Scholes Model (Bad model, I know)
+    to calculate the implied volatility of all the
+    options in a given option chain.
+    
+    Parameters
+    ----------
+    option_chain : pd.DataFrame
+        DataFrame with MultiIndex that contains
+        all options for all expirations 
+    
+    Returns
+    -------
+    vol_df : pd.DataFrame
+        Probably a DataFrame with volatility, time to
+        maturity, and strike price, organized by option
+        type
+    """
     option_chain = option_chain.copy()
 
-
-    return
-    
-def calculate_implied_vol(option_chain):
 
     return
